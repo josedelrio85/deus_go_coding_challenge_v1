e8c29d3d0a036ae558d09110dae89eee048903b7
josedelrio85 <josedelrio85@gmail.com>
2022-04-27 10:58:22 +0200

    refactoring to implement singleton pattern to storer and improved getter function removing the for loop

--- pkg/utils.go
+++ pkg/utils.go
@@ -1,23 +1,45 @@
 package deus_cc
 
 import (
+	"log"
 	"net/url"
+	"sync"
 
 	"github.com/google/uuid"
 )
 
-var events = make(map[Event]bool)
+type Storer struct {
+	events  map[Event]bool
+	counter map[string]int
+}
+
+var singleton *Storer
+var once sync.Once
+
+func GetStorer() *Storer {
+	once.Do(func() {
+		singleton = &Storer{
+			events:  make(map[Event]bool),
+			counter: make(map[string]int),
+		}
+	})
+	return singleton
+}
 
 // SetTestEvents is used to "mock" the event collector. Used for testing only
-func SetTestEvents(input map[Event]bool) {
-	events = input
+func SetTestEvents(events []Event) {
+	for _, event := range events {
+		Adder(event)
+	}
 }
 
 // Adder is the responsible to add (or not) an event to the event collector
 // Use an Event struct as an input and return a boolean
 func Adder(event Event) bool {
-	if !events[event] {
-		events[event] = true
+	storer := GetStorer()
+	if !storer.events[event] {
+		storer.events[event] = true
+		storer.counter[event.Url]++
 		return true
 	}
 	return false
@@ -27,13 +49,18 @@ func Adder(event Event) bool {
 // with the url provided as input param
 // Use an string as an input and return a int
 func Getter(url string) int {
-	counter := 0
-	for ev := range events {
-		if ev.Url == url {
-			counter++
-		}
-	}
-	return counter
+	storer := GetStorer()
+	log.Println(url)
+	log.Println(storer.counter[url])
+	return storer.counter[url]
+	// log.Println(storer.events)
+	// log.Println(storer.counter[url])
+	// for ev := range storer.events {
+	// 	if ev.Url == url {
+	// 		counter++
+	// 	}
+	// }
+	// return counter
 }
 
 func ValidateData(event Event) (bool, error) {
