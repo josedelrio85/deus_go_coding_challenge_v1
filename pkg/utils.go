94dc83a8a5d1c4fa0dd8b7840868b4cd854c5d4b
josedelrio85 <josedelrio85@gmail.com>
2022-04-27 12:18:32 +0200

    created mutex support for read/write operations

--- pkg/utils.go
+++ pkg/utils.go
@@ -10,6 +10,7 @@ import (
 type Storer struct {
 	events  map[Event]bool
 	counter map[string]int
+	mutex   *sync.Mutex
 }
 
 var singleton *Storer
@@ -20,25 +21,27 @@ func GetStorer() *Storer {
 		singleton = &Storer{
 			events:  make(map[Event]bool),
 			counter: make(map[string]int),
+			mutex:   &sync.Mutex{},
 		}
 	})
 	return singleton
 }
 
 // SetTestEvents is used to "mock" the event collector. Used for testing only
-func SetTestEvents(events []Event) {
+func (s *Storer) SetTestEvents(events []Event) {
 	for _, event := range events {
-		Adder(event)
+		s.Adder(event)
 	}
 }
 
 // Adder is the responsible to add (or not) an event to the event collector
 // Use an Event struct as an input and return a boolean
-func Adder(event Event) bool {
-	storer := GetStorer()
-	if !storer.events[event] {
-		storer.events[event] = true
-		storer.counter[event.Url]++
+func (s *Storer) Adder(event Event) bool {
+	if !s.events[event] {
+		s.mutex.Lock()
+		s.events[event] = true
+		s.counter[event.Url]++
+		s.mutex.Unlock()
 		return true
 	}
 	return false
@@ -47,9 +50,10 @@ func Adder(event Event) bool {
 // Getter is the responsible to return the number of distinct visitors relate
 // with the url provided as input param
 // Use an string as an input and return a int
-func Getter(url string) int {
-	storer := GetStorer()
-	return storer.counter[url]
+func (s *Storer) Getter(url string) int {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	return s.counter[url]
 }
 
 func ValidateData(event Event) (bool, error) {
